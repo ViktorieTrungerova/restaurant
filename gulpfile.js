c3e11ec46f4dc724b7cb99d69d84226cb123986a
Unknown <vikiaondra@gmail.com>
2018-06-22 15:43:31 +0200

    zacatek

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+
+gulp.task('sass', function(){
+    return gulp.src('www/assets/css/style.scss')
+        .pipe(sass()) // Using gulp-sass
+        .pipe(gulp.dest('www/assets/css/'))
+});
+
+
+gulp.task('cssmin', function () {
+    gulp.src('www/assets/css/style.css')
+        .pipe(cssmin())
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest('www/assets/css'));
+});
+
+gulp.task('watch',function () {
+    gulp.watch('www/assets/css/**/*.scss', ['sass', 'cssmin']);
+});
